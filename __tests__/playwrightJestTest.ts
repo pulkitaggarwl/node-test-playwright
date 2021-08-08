715af8670e99cade2ac681064869df44bb9d7d02
pulkitaggarwl <36596168+pulkitaggarwl@users.noreply.github.com>
2021-08-08 15:17:36 +0530

    Update playwrightJestTest.ts

--- __tests__/playwrightJestTest.ts
+++ __tests__/playwrightJestTest.ts
@@ -1,46 +1,12 @@
 const playwright = require('playwright');
-const { webkit, devices } = require('playwright');
 
-describe('Habrahabr', () => {
-    let page, browser;
-
-    beforeAll(async () => {
-        for (const browserType of ['chromium']) {
-            browser = await playwright[browserType].launch({headless:false});
-            const context = await browser.newContext();
-            page = await context.newPage();
-            await page.goto('https://habr.com');
-        }
-    });
-
-    it('should be titled "Habr"', async () => {
-        await expect(page.title()).resolves.toBe('Лучшие публикации за сутки / Хабр');
-    });
-
-    afterAll(async () => {
-        await browser.close();
-    })
-});
-
-describe('Geo location and navigation in yandex maps', () => {
-    const iPhone11 = devices['iPhone 11 Pro'];
-    let browser;
-    it('should navigate and take a screenshot"', async () => {
-        browser = await webkit.launch({headless:false});
-        const context = await browser.newContext({
-            viewport: iPhone11.viewport,
-            userAgent: iPhone11.userAgent,
-            geolocation: { longitude: 12.492507, latitude: 41.889938 },
-            permissions: { 'https://yandex.ru/maps': ['geolocation'] }
-        });
-        const page = await context.newPage();
-        await page.goto('https://yandex.ru/maps');
-        await page.screenshot({ path: 'iphone.png' });
-    });
-
-    afterAll(async () => {
-        await browser.close();
-    })
-});
-
-
+(async () => {
+  for (const browserType of ['chromium', 'firefox', 'webkit']) {
+    const browser = await playwright[browserType].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://whatsmyuseragent.org/');
+    await page.screenshot({ path: `example-${browserType}.png` });
+    await browser.close();
+  }
+})();

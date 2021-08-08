43a77429baa1dbd1a894aafc6420116baa846034
pulkitaggarwl <36596168+pulkitaggarwl@users.noreply.github.com>
2021-08-08 15:36:41 +0530

    Update playwrightJestTest.ts

--- __tests__/playwrightJestTest.ts
+++ __tests__/playwrightJestTest.ts
@@ -1,12 +1,68 @@
-const playwright = require('playwright');
+const { chromium } = require('playwright')
+const assert = require('assert')
+const { Eyes, ClassicRunner, Target, RectangleSize} = require('@applitools/eyes-playwright')
 
-(async () => {
-  for (const browserType of ['chromium', 'firefox', 'webkit']) {
-    const browser = await playwright[browserType].launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://whatsmyuseragent.org/');
-    await page.screenshot({ path: `example-${browserType}.png` });
-    await browser.close();
-  }
-})();
+describe('Automation Bookstore', function () {
+	let browser,  page
+	const eyes = new Eyes(new ClassicRunner())
+
+	before(async () => {
+		browser = await chromium.launch()
+	})
+	after(async () => {
+		await browser.close()
+	})
+
+	beforeEach(async () => {
+		page = await browser.newPage()
+		await page.goto('https://automationbookstore.dev/')
+	})
+	afterEach(async () => {
+		await page.close()
+		await eyes.abortIfNotClosed()
+	})
+
+
+	it('should have title', async () => {
+		assert.equal(await page.innerText('#page-title'), 'Automation Bookstore')
+	})
+
+	it('should return one book when exact title is given', async () => {
+		await page.fill('#searchBar', 'Agile Testing')
+		await page.waitForSelector('li.ui-screen-hidden', { state: 'attached' })
+
+		const visibleBooksSelector = 'li:not(.ui-screen-hidden)'
+		const visibleBooks = await page.$$(visibleBooksSelector)
+		assert.equal(visibleBooks.length, 1)
+		assert.equal(await page.innerText(visibleBooksSelector + ' >> h2'), 'Agile Testing')
+	})
+
+	it('should return multiple books when partial title is given', async () => {
+		await page.fill('#searchBar', 'test')
+		await page.waitForSelector('li.ui-screen-hidden', { state: 'attached' })
+
+		const expectedBooks = [
+			'Test Automation in the Real World',
+			'Experiences of Test Automation',
+			'Agile Testing',
+			'How Google Tests Software',
+			'Java For Testers']
+
+		const visibleBooksSelector = 'li:not(.ui-screen-hidden)'
+		const visibleBooks = await page.$$(visibleBooksSelector)
+		assert.equal(visibleBooks.length, expectedBooks.length)
+		expectedBooks.forEach(book => async () => {
+			assert.equal(await page.innerText(visibleBooksSelector + ' >> h2'), book)
+		})
+	})
+
+	it('should be visually perfect', async () => {
+		await page.fill('#searchBar', 'test')
+		await page.waitForSelector('li.ui-screen-hidden', { state: 'attached' })
+
+		//Visual Testing
+		await eyes.open(page, 'Automation Bookstore - Playwright', 'Partial Search', new RectangleSize(800, 600))
+		await eyes.check(Target.window().fully())
+		await eyes.close()
+	})
+})
